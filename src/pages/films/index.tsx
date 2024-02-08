f8358e57c09d0cb60cb5a6c865b547d2db388a72
Shamil <khanmag329@gmail.com>
2024-02-08 20:24:27 +0300

    mw

--- src/pages/films/index.tsx
+++ src/pages/films/index.tsx
@@ -1,18 +1,43 @@
-import { useEffect, useState } from "react";
-import { FilmType, getSWFilms } from "../../api/SWAPI";
+import {
+  useCallback,
+  useEffect,
+  //  useState 
+} from "react";
+import {
+  // FilmType, 
+  getSWFilms
+} from "../../api/SWAPI";
 import FilmCard from "./FilmCard";
+// import { useDispatch, useSelector } from "react-redux";
+import { 
+  // RootState, 
+  useAppDispatch, 
+  useAppSelector 
+} from "../../store";
+import { setFilmsList } from "../../store/films/filmsSlice";
 
 const FilmsPage = () => {
-  const [filmsList, setFilmsList] = useState<FilmType[]>([])
+  // const [filmsList, setFilmsList] = useState<FilmType[]>([])
+  // const filmsList = useSelector((store: RootState) => store.films.filmsList)
+  const filmsList = useAppSelector(store => store.films.filmsList)
+  // const dispatch = useDispatch()
+  const dispatch = useAppDispatch()
 
-  const getFilmsList = async () => {
+  // const getFilmsList = async () => {
+  //   const data = await getSWFilms()
+  //   //  setFilmsList(data)
+  //   dispatch(setFilmsList(data))
+  // }
+
+  const getFilmsList = useCallback(async () => {
     const data = await getSWFilms()
-    setFilmsList(data)
-  }
+    dispatch(setFilmsList(data))
+  }, [dispatch])
 
   useEffect(() => {
     getFilmsList()
-  }, [])
+  }, [getFilmsList])
+  // typeof func - object
 
   // const obj =  {
   //     "key": "film.episode_id",
@@ -30,11 +55,11 @@ const FilmsPage = () => {
             key={film.episode_id}
             title={film.title}
             director={film.director}
-            // {
-            //   key: film.episode_id,
-            //   title: film.title,
-            //   director: film.director,
-            // }
+          // {
+          //   key: film.episode_id,
+          //   title: film.title,
+          //   director: film.director,
+          // }
           />
         ))
       }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import filmsSlice from "./films/filmsSlice";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 
 
 const store = configureStore({
@@ -10,3 +11,9 @@ const store = configureStore({
 
 
 export default store
+
+export type RootState = ReturnType<typeof store.getState>
+type AppDispatch = typeof store.dispatch
+
+export const useAppSelector: TypedUseSelectorHook< RootState> = useSelector
+export const useAppDispatch: () => AppDispatch = useDispatch
