d57b6521ec399537b3573df83d306f03ca7c4d25
Shamil <khanmag329@gmail.com>
2024-02-12 19:39:07 +0300

    mw

--- src/component/PlanetList/index.tsx
+++ src/component/PlanetList/index.tsx
@@ -3,17 +3,44 @@ import PlanetIcon from "../../assets/icons/PlanetIcon";
 
 const PlanetList = ({ planets }: { planets: string[] }) => {
   const [showItems, setShowItems] = useState(false)
+  const [currentClass, setCurrentClass] = useState<"planetShower" | "planetHiddener">("planetHiddener")
+  const [isLoading, setIsLoading] = useState(false)
+
+  const showElement = () => {
+    if (isLoading) return null
+    setIsLoading(true)
+    setShowItems(true)
+    setCurrentClass("planetShower")
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 500)
+  }
+  const hideElement = () => {
+    if (isLoading) return null
+    setIsLoading(true)
+    setCurrentClass("planetHiddener")
+    setTimeout(() => {
+      setShowItems(false)
+      setIsLoading(false)
+    }, 500)
+  }
   return (
-    <div>
+    <div style={{ width: "200px" }}>
       <div
         style={{ display: "flex", alignItems: "center", cursor: "pointer" }}
-        onClick={() => { setShowItems(prev => !prev) }}
+        onClick={() => {
+          if (!showItems) {
+            showElement()
+          } else {
+            hideElement()
+          }
+        }}
       >
         <PlanetIcon />
         <h5>PLANETS</h5>
-      </div>
+      </div >
       {showItems && (
-        <ul>
+        <ul className={currentClass}>
           {planets.map(item => (
             <li key={item}>{item}</li>
           ))}
