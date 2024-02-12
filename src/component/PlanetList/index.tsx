9dcb627b94f6bda928db8de050c79d7860735293
Shamil <khanmag329@gmail.com>
2024-02-12 20:08:59 +0300

    mw

--- src/component/PlanetList/index.tsx
+++ src/component/PlanetList/index.tsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import PlanetIcon from "../../assets/icons/PlanetIcon";
 import { Transition } from "react-transition-group";
+import { PlanetType } from "../../api/SWAPI";
 
 const PlanetList = ({ planets }: { planets: string[] }) => {
   const [showItems, setShowItems] = useState(false)
@@ -36,7 +37,11 @@ const PlanetList = ({ planets }: { planets: string[] }) => {
           (state) => (
             <ul className={`planetList ${state}`}>
             {planets.map(item => (
-              <li key={item}>{item}</li>
+              // <li key={item}>{item}</li>
+              <PlanetItem 
+                key={item}
+                url={item}
+              />
             ))}
           </ul>
           )
@@ -47,3 +52,22 @@ const PlanetList = ({ planets }: { planets: string[] }) => {
 };
 
 export default PlanetList;
+
+
+const PlanetItem = ({url}:{url: string}) => {
+  const [name, setName] = useState<string | null>(null)
+
+  useEffect( () => {
+    try {
+      fetch(url)
+      .then( response => {
+        if (response.status !== 200) throw new Error("fetch error")
+        return response.json()
+      })
+      .then( (data: PlanetType) => setName(data.name))
+    } catch (err) {
+      if (err instanceof Error) console.error(err.message);
+    }
+  }, [url])
+  return <li>{name}</li>
+}
